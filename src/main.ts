2e0c24efb2c9a6e1214099aa19c415663e7fb07a
andresMucino <mucino.andres1990@gmail.com>
2022-01-25 10:52:19 -0600

    add use global

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({});
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
